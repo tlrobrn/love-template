a023144221df4e936904e682c4762538b01e7f2e
Taylor O'Brien <tlrobrn@users.noreply.github.com>
2023-11-17 14:50:01 -0700

    entry point for love

---
+++ main.lua
@@ -0,0 +1,11 @@
+function love.load()
+  -- TODO: implement code to run on app load
+end
+
+function love.update(dt)
+  -- TODO: implement code to run on each game loop
+end
+
+function love.draw()
+  -- TODO: implement code to run to render each loop
+end
